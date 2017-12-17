70262a8f087257a2d0f74f31b107ea3d17793f78
Your Actual Name, In Quotes <serpil60@hotmail.fr>
2017-12-17 14:08:52 +0100

    errorrs

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,8 +2,8 @@ class Search < ApplicationRecord::Base
 
 def courses_search
 
-courses = courses.where(["categorie LIKE ?", categorie]) if categorie.present:?
-courses = courses.where(["typdedoc LIKE ?", typedoc ]) if typedoc.present:?
+courses = courses.where(["categorie LIKE ?", categorie]) if categorie.present?
+courses = courses.where(["typdedoc LIKE ?", typedoc ]) if typedoc.present?
 
 return courses
 
